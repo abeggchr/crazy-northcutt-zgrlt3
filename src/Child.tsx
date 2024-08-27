8e7e4de667b28e410c50daa371e29a95c5305284
Christian Abegg <christian.abegg@zuehlke.com>
2024-08-27 17:30:32 +0200

    simple tracking example

--- src/Child.tsx
+++ src/Child.tsx
@@ -1,5 +1,9 @@
+import { useTracking } from "react-tracking";
+
 export function Child() {
+    var {trackEvent} = useTracking<any>({child: "child"});
+    
     return (
-        <button type='button'>Child</button>
+        <button type='button' onClick={() => trackEvent({event: 'click in Child'})}>Child</button>
     )
 }
--- src/Shell.tsx
+++ src/Shell.tsx
@@ -1,10 +1,13 @@
+import { useTracking } from "react-tracking";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
 export function Shell() {
+    var {trackEvent} = useTracking<any>({shell: "shell"});
+
     return (
         <>
-            <button type='button'>Shell</button>
+            <button type='button' onClick={() => trackEvent({event: 'click in Shell'})}>Shell</button>
             <br />
             <br />
             <Outlet />
